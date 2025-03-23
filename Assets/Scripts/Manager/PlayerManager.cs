8261df651b8ba364c202d34c8d03b1706cdd93e6
Firiosair <jombarthippolyte@gmail.com>
2025-03-23 21:59:53 +0100

    Ajout du système de progressions d'xp par niveau

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class PlayerManager : MonoBehaviour
     public int playerMana = 100;
     public int maxPlayerMana = 100;
     public float playerXP = 0;
+    public float playerLevel = 0;
+    public Dictionary<int, int> xpAssociator = new Dictionary<int, int>(){{0,0},{1,100},{2,250},{3,500},{4,800},{5,1200},{6,1700},{7,2300},{8,3000},{9,4000},{10,5000}};
     void Awake()
     {
         if (instance == null){instance = this;}
@@ -33,6 +36,7 @@ public class PlayerManager : MonoBehaviour
         playerMana-=manaUsed;
         ManaCheck();
     }
+    private void AddXp(int x) {playerXP+=x;}
     private void HealthCheck()
     {
         if (playerHealth < 0){playerHealth=0;}
@@ -43,5 +47,8 @@ public class PlayerManager : MonoBehaviour
         if (playerMana < 0){playerMana=0;}
         if (playerMana > maxPlayerMana){playerMana=maxPlayerHealth;}
     }
-    private void AddXp(int x) {playerXP+=x;}
+    private void XpCheck()
+    {
+        if (xpAssociator[playerLevel+1] < playerXP){playerLevel+=1;}
+    }
 }
